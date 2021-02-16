fd32bee8f21d400e1855494ea0dda357dced579e
“Anonna_Ferdaus” <anonnatahsin132@gmail.com>
2021-02-16 18:54:09 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+
+   var x string = "Hellow world"
+   
+   
+   func main(){
+   
+   fmt.Println(x)
+   
+   
+   }
+   
+   func f() {
+   
+     fmt.Println(x)
+   
+   }
+
+
+
